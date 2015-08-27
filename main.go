cb65c95861b7a77bd793f39fe519d0824e3bf7fa
Fábio Gomes <fabioxgn@gmail.com>
2015-08-27 14:11:42 -0300

    Changes IRC connection to slack

--- main.go
+++ main.go
@@ -1,59 +1,20 @@
 package main
 
 import (
-	"log"
 	"os"
-	"strings"
 
 	"github.com/fabioxgn/go-bot"
 	_ "github.com/fabioxgn/go-bot/commands/catfacts"
 	_ "github.com/fabioxgn/go-bot/commands/catgif"
 	_ "github.com/fabioxgn/go-bot/commands/chucknorris"
 	_ "github.com/fabioxgn/go-bot/commands/cotacao"
-	_ "github.com/fabioxgn/go-bot/commands/example"
+	_ "github.com/fabioxgn/go-bot/commands/dilma"
 	_ "github.com/fabioxgn/go-bot/commands/gif"
 	_ "github.com/fabioxgn/go-bot/commands/godoc"
-	_ "github.com/fabioxgn/go-bot/commands/jira"
 	_ "github.com/fabioxgn/go-bot/commands/megasena"
 	_ "github.com/fabioxgn/go-bot/commands/puppet"
-	_ "github.com/fabioxgn/go-bot/commands/dilma"
 )
 
 func main() {
-	config := newConfig()
-	log.Printf("%#v\n", config)
-	bot.Run(config)
-}
-
-func newConfig() *bot.Config {
-	if os.Getenv("ENV") == "production" {
-		return productionConfig()
-	} else {
-		return developmentConfig()
-	}
-
-}
-
-func productionConfig() *bot.Config {
-	return &bot.Config{
-		Server:        os.Getenv("IRC_SERVER"),
-		Channels:      strings.Split(os.Getenv("IRC_CHANNELS"), ","),
-		User:          os.Getenv("IRC_USER"),
-		Nick:          os.Getenv("IRC_NICK"),
-		Password:      os.Getenv("IRC_PASSWORD"),
-		UseTLS:        true,
-		TLSServerName: os.Getenv("IRC_TLSServerName"),
-		Debug:         os.Getenv("DEBUG") != "",
-	}
-}
-
-func developmentConfig() *bot.Config {
-	return &bot.Config{
-		Server:   "irc.freenode.net:6697",
-		Channels: []string{"#go-bot"},
-		User:     "go-bot-dev",
-		Nick:     "go-bot-dev",
-		UseTLS:   true,
-		Debug:    true,
-	}
+	bot.RunSlack(os.Getenv("SLACK_API_TOKEN"))
 }
